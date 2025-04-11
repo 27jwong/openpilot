5aa632232fe7649963e9101b7d8d89b69458d32b
27jwong <guineapig9@gmail.com>
2025-04-11 14:51:39 -0400

    Update interface.py

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -59,7 +59,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kiV = [1.0, 0.8, 0.5]
       # ret.longitudinalActuatorDelay = 0.05
       ret.startingState = True
-      ret.steerActuatorDelay = 0.1
+      ret.steerActuatorDelay = 0.12
       if p.get_bool("ManualTransmission"):
         ret.flags |= MazdaFlags.MANUAL_TRANSMISSION.value
 
