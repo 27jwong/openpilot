f8dc082c117f5e4e0ebb92459a87fb2f259dcacd
27jwong <guineapig9@gmail.com>
2025-04-29 18:09:58 -0400

    Revert "Tomb Raider 5"
    This reverts commit ef9fa9e44d640927523beebad86faca820cd5860.

--- selfdrive/frogpilot/ui/qt/offroad/model_settings.cc
+++ selfdrive/frogpilot/ui/qt/offroad/model_settings.cc
@@ -198,7 +198,6 @@ FrogPilotModelPanel::FrogPilotModelPanel(FrogPilotSettingsWindow *parent) : Frog
             selectableModels.append(modelName);
           }
         }
-        selectableModels.append(modelFileToNameMap.value("tomb-raider"));
         selectableModels.sort();
         selectableModels.prepend(modelFileToNameMap.value(QString::fromStdString(params_default.get("Model"))));
 
@@ -266,9 +265,6 @@ void FrogPilotModelPanel::showEvent(QShowEvent *event) {
     modelFileToNameMap.insert(availableModels[i], availableModelNames[i]);
     modelFileToNameMapProcessed.insert(availableModels[i], processModelName(availableModelNames[i]));
   }
-
-  modelFileToNameMap.insert("tomb-raider", "Tomb Raider 👀📡");
-  modelFileToNameMapProcessed.insert("tomb-raider", "Tomb Raider");
 
   downloadableModels = availableModelNames;
   for (const QString &file : modelDir.entryList(QDir::Files)) {
