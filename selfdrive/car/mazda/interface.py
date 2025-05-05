0f4ac7d6bd66f3a9a17007cec883dda6b8e04e66
27jwong <guineapig9@gmail.com>
2025-05-04 23:22:57 -0400

    Update interface.py

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -15,7 +15,7 @@ EventName = car.CarEvent.EventName
 
 NON_LINEAR_TORQUE_PARAMS = {
   CAR.MAZDA_3_2019: (3.8818, 0.6873, 0.0999, 0.3605),
-  CAR.MAZDA_CX_30: (5, 0.75, 0.0999, 0.3605),
+  CAR.MAZDA_CX_30: (4, 0.6873, 0.0999, 0.3605),
 }
 
 class CarInterface(CarInterfaceBase):
