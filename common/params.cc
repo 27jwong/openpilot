4f0cbf9e0c91339e9addd77573b77f9855e7c33a
27jwong <guineapig9@gmail.com>
2025-05-10 19:47:24 -0400

    Revert "WebRTC Live Streamer"
    This reverts commit e3e414e4d12f0f1845e79e391d21e1b2399a230d.

--- common/params.cc
+++ common/params.cc
@@ -565,8 +565,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"NoFSC", PERSISTENT},
     {"BlendedACC", PERSISTENT},
     {"ManualTransmission", PERSISTENT},
-    {"LiveStreamRunning", CLEAR_ON_MANAGER_START},
-    {"RemoteAccess", CLEAR_ON_MANAGER_START},
 };
 
 } // namespace
