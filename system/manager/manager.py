60725349b5bf370ad4f7922e4811e46adeb77043
MoreTore <ryleymccarroll22@gmail.com>
2025-05-21 12:22:09 -0500

    params

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -48,6 +48,7 @@ def manager_init() -> None:
     ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
     ("BlendedACC", "0"),
     ("RecordRoad", "1"),
+    ("RemoteAccess", "0"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
