4c2fab8b1b2da18ab9064b77e2382d8f13346187
Jeffrey Wong <guineapig9@gmail.com>
2025-10-25 14:24:53 -0400

    Update torque parameters for MAZDA_CX_30

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -16,7 +16,7 @@ EventName = car.CarEvent.EventName
 
 NON_LINEAR_TORQUE_PARAMS = {
   CAR.MAZDA_3_2019: (3.650, 1.0, 0.13, 0.3605),
-  CAR.MAZDA_CX_30: (4.68689, 0.79999, 0.18244, 0.38763),
+  CAR.MAZDA_CX_30: (8.24763, 0.86120, 0.16532 0.27521),
 }
 
 class CarInterface(CarInterfaceBase):
