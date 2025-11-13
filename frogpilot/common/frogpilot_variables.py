7f3865e314a034e4ab90171b1603440c7835f8dc
27jwong <guineapig9@gmail.com>
2025-11-13 12:57:40 -0500

    neurips-driving 11/13

--- frogpilot/common/frogpilot_variables.py
+++ frogpilot/common/frogpilot_variables.py
@@ -76,7 +76,7 @@ MAPS_PATH = Path("/data/media/0/osm/offline")
 NNFF_MODELS_PATH = Path(BASEDIR) / "frogpilot/assets/nnff_models"
 
 DEFAULT_MODEL = "firehose"
-DEFAULT_MODEL_NAME = "Firehose (Default) 👀📡"
+DEFAULT_MODEL_NAME = "neurips-driving 👀📡"
 DEFAULT_MODEL_VERSION = "v9"
 
 BUTTON_FUNCTIONS = {
