67f9a0d1baf71997787792cbd61b2b83368caf92
Jeffrey Wong <guineapig9@gmail.com>
2025-12-09 13:10:41 -0500

    Update fan_controller.py

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -2,8 +2,8 @@
 from abc import ABC, abstractmethod
 
 from openpilot.common.realtime import DT_HW
-from openpilot.common.numpy_fast import interp
 from openpilot.common.swaglog import cloudlog
+from openpilot.common.numpy_fast import interp
 from openpilot.selfdrive.controls.lib.pid import PIDController
 
 class BaseFanController(ABC):
@@ -21,16 +21,16 @@ class TiciFanController(BaseFanController):
     self.controller = PIDController(k_p=0, k_i=4e-3, rate=(1 / DT_HW))
 
   def update(self, cur_temp: float, ignition: bool) -> int:
-    self.controller.neg_limit = -(100 if ignition else 30)
-    self.controller.pos_limit = -(30 if ignition else 0)
+    self.controller.pos_limit = 100 if ignition else 30
+    self.controller.neg_limit = 30 if ignition else 0
 
     if ignition != self.last_ignition:
       self.controller.reset()
 
-    error = 70 - cur_temp
-    fan_pwr_out = -int(self.controller.update(
+    error = cur_temp - 75
+    fan_pwr_out = int(self.controller.update(
                       error=error,
-                      feedforward=interp(cur_temp, [60.0, 100.0], [0, -100])
+                      feedforward=interp(cur_temp, [60.0, 100.0], [0, 100])
                     ))
 
     self.last_ignition = ignition
